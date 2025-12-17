5f3fdddcb738e264548c7974cc1b11fc07271197
Gruprodigi <113732094+Gruprodigi@users.noreply.github.com>
2025-12-16 21:04:26 -0300

    deply error

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Garante caminhos relativos corretos no build
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
+  }
 })
